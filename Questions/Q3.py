442038bd5958747ead5b2a9fef78c19c231a99ed
Devanshu-1105 <devanshugarad1@gmail.com>
2025-09-20 00:41:46 +0530

    Arithematic Question

--- Questions/Q3.py
+++ Questions/Q3.py
@@ -5,7 +5,7 @@ The provided code stub reads two integers from STDIN,  and . Add code to print t
 The first line contains the sum of the two numbers.
 The second line contains the difference of the two numbers (first - second).
 The third line contains the product of the two numbers."""
- 
+ #swed
 # #taking the user input 
 
 input_1 = int(input("Number 1: "))
