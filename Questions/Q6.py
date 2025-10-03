e880058dcb39057e6bcd1102e893e85927b94858
Devanshu-1105 <devanshugarad1@gmail.com>
2025-10-03 23:50:28 +0530

    Nested lists

---
+++ Questions/Q6.py
@@ -0,0 +1,15 @@
+students = [["Dharma", 100.2],
+           ["Karma", 100.1],
+           ["Ching", 100],
+           ]
+
+grades = [score for name, score in students]
+
+# unique_grades = sorted(set(grades))
+# second_lowest = unique_grades[1]
+
+# names = [name for name, score in students if score == second_lowest]
+# names.sort()
+
+# for n in names:
+#     print(n)
